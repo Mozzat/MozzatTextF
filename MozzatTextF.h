f0a7d2f88139c8fd0d9715b483f82c566b1615ad
Mozzat <taogroups@163.com>
2018-12-19 23:21:45 +0800

    初始化代码

---
+++ MozzatTextF.h
@@ -0,0 +1,17 @@
+//
+//  MozzatTextF.h
+//  MozzatTextF
+//
+//  Created by taotingxing on 2018/12/19.
+//  Copyright © 2018 陶庭兴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MozzatTextF : UITextField
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MozzatTextF.m
@@ -0,0 +1,22 @@
+
+//
+//  MozzatTextF.m
+//  MozzatTextF
+//
+//  Created by taotingxing on 2018/12/19.
+//  Copyright © 2018 陶庭兴. All rights reserved.
+//
+
+#import "MozzatTextF.h"
+
+@implementation MozzatTextF
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
